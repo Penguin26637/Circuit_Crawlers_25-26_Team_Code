ef869935deb0bd269c8ef90916c51faac08dc6ae
Penguin26637 <gsellinghaus@gmail.com>
2026-01-23 17:49:49 -0800

    Some Test Files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rebuilt/Test_Programs/Gregory_Test_Files/ServoClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rebuilt/Test_Programs/Gregory_Test_Files/ServoClass.java
@@ -2,43 +2,75 @@ package org.firstinspires.ftc.teamcode.Rebuilt.Test_Programs.Gregory_Test_Files;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 /**
- * Represents an FTC servo with two preset positions.
+ * Represents an FTC servo that can be positioned at any value.
+ * Supports both standard Servo and CRServo types.
  */
-class ServoPreset {
-    private String hardwareMapName;
-    private double pos1;
-    private double pos2;
-    private Servo servo; // The actual FTC Hardware Object
+class ServoClass {
+    public enum ServoType {
+        STANDARD_SERVO,
+        CONTINUOUS_SERVO
+    }
 
-    public ServoPreset(String name, double pos1, double pos2) {
+    private String hardwareMapName;
+    private ServoType type;
+    private Servo servo;           // For standard servos
+    private CRServo crServo;       // For continuous rotation servos
+
+    public ServoClass(String name, ServoType type) {
         this.hardwareMapName = name;
-        this.pos1 = pos1;
-        this.pos2 = pos2;
+        this.type = type;
     }
 
     /**
      * Initializes the servo hardware from the hardwareMap.
      */
     public void init(HardwareMap hwMap) {
-        servo = hwMap.get(Servo.class, hardwareMapName);
+        if (type == ServoType.STANDARD_SERVO) {
+            servo = hwMap.get(Servo.class, hardwareMapName);
+        } else {
+            crServo = hwMap.get(CRServo.class, hardwareMapName);
+        }
     }
 
-    // Command methods
-    public void goToPosition1() {
-        if (servo != null) servo.setPosition(pos1);
+    /**
+     * Sets the servo to a specific position (0.0 to 1.0) or power (-1.0 to 1.0 for CRServo).
+     * @param position The position/power to set
+     */
+    public void goToPosition(double position) {
+        if (type == ServoType.STANDARD_SERVO && servo != null) {
+            servo.setPosition(position);
+        } else if (type == ServoType.CONTINUOUS_SERVO && crServo != null) {
+            crServo.setPower(position);
+        }
     }
 
-    public void goToPosition2() {
-        if (servo != null) servo.setPosition(pos2);
+    /**
+     * Stops a continuous rotation servo (sets power to 0).
+     * Has no effect on standard servos.
+     */
+    public void stop() {
+        if (type == ServoType.CONTINUOUS_SERVO && crServo != null) {
+            crServo.setPower(0.0);
+        }
     }
 
-    // Getters and Setters
-    public double getPos1() { return pos1; }
-    public void setPos1(double pos1) { this.pos1 = pos1; }
-    public double getPos2() { return pos2; }
-    public void setPos2(double pos2) { this.pos2 = pos2; }
+    /**
+     * Gets the current position (standard servo only).
+     * @return Current position, or -1 if not a standard servo
+     */
+    public double getCurrentPosition() {
+        if (type == ServoType.STANDARD_SERVO && servo != null) {
+            return servo.getPosition();
+        }
+        return -1;
+    }
+
+    // Getters
     public String getName() { return hardwareMapName; }
-    public Servo getHardware() { return servo; }
+    public ServoType getType() { return type; }
+    public Servo getServo() { return servo; }
+    public CRServo getCRServo() { return crServo; }
 }
